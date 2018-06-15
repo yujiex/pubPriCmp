55646b2a683942b0b33cf602337a70e5abf9d4f6
yujiex <shenqing1225@gmail.com>
2018-06-15 13:27:49 -0400

    Initial commit

---
+++ R/compile_raw_data.R
@@ -0,0 +1,14 @@
+## fixme: figure out the source of the location for PNC data
+#' Copy raw file from external
+#'
+#' This function copies in raw data files from an external working directory
+#' @keywords copy raw
+#' @export
+#' @examples
+#' get_nearby_isd_stations(lat_lon_df, isd_data=isd_data, radius = 100, limit=5,
+copy_raw <- function () {
+  ## get energy file
+  df = readr::read_csv("~/Dropbox/thesis/writeups/policy_cmp/tables/all_energy.csv") %>%
+    as.data.frame() %>%
+    {.}
+}
