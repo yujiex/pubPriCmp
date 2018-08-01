8378ba1706e0b683bb53b025fa27782883071b71
yujiex <shenqing1225@gmail.com>
2018-08-01 13:02:41 -0400

    Copy data files to latex folder

--- R/compile_raw_data.R
+++ R/compile_raw_data.R
@@ -8,7 +8,11 @@
 #' get_nearby_isd_stations(lat_lon_df, isd_data=isd_data, radius = 100, limit=5,
 copy_raw <- function () {
   ## get energy file
+  devtools::use_data_raw(pkg="~/Dropbox/thesis/code/pubPriCmp")
+  inputdir = "~/Dropbox/thesis/writeups/policy_cmp/tables"
+  outputdir = "~/Dropbox/thesis/code/"
   df = readr::read_csv("~/Dropbox/thesis/writeups/policy_cmp/tables/all_energy.csv") %>%
     as.data.frame() %>%
+    readr::write_csv("~/Dropbox/thesis/writeups/policy_cmp/tables/%s", filename)
     {.}
 }
